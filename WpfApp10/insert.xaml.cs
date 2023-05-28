f9ce7caa3d8ac476be4b440db6292e9fa5116c37
Hesen_ch75@itstep.edu.az <Hesen_ch75@itstep.edu.az>
2023-05-28 23:57:42 +0400

    sas

--- WpfApp10/insert.xaml.cs
+++ WpfApp10/insert.xaml.cs
@@ -10,12 +10,10 @@ namespace WpfApp10
     {
         SqlConnection conn;
         string cs = "";
-
         public insert()
         {
             InitializeComponent();
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             using (SqlConnection conn = new SqlConnection())
@@ -23,31 +21,21 @@ namespace WpfApp10
                 cs = ConfigurationManager.ConnectionStrings["myconn"].ConnectionString;
                 conn.ConnectionString = cs;
                 conn.Open();
-
                 string insertQuery = "INSERT INTO Authors (Id,Firstname, Lastname) VALUES (@id,@firstName, @lastName)";
-
                 using (SqlCommand command = new SqlCommand(insertQuery, conn))
                 {
-            
                     command.Parameters.AddWithValue("@id", idtextbox.Text);
                     command.Parameters.AddWithValue("@firstName", firstnametextbox.Text);
-                    command.Parameters.AddWithValue("@lastName", lastnametextbox.Text);
-
-                  
+                    command.Parameters.AddWithValue("@lastName", lastnametextbox.Text); 
                     command.ExecuteNonQuery();
                 }
-
- 
                 conn.Close();
             }
-
             idtextbox.Clear();
             firstnametextbox.Clear();
             lastnametextbox.Clear();
-
             MessageBox.Show("Elave edildi");
         }
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Close();
